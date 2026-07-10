30c8cd8e070201043b85c53189ea2912d9a14170
Nathan Nye <nathan@nye.design>
2026-07-10 13:46:25 -0600

    color swap

--- src/report/components/Header.tsx
+++ src/report/components/Header.tsx
@@ -15,17 +15,10 @@ export function Header({ data }: HeaderProps) {
 
 	return (
 		<header class="mb-24 flex pt-[24vh] flex-wrap items-start justify-between gap-16 lg:items-end">
-			<div class="flex items-center gap-80 mb-90">
-				<p>
-					Report from {formattedDate}
-				</p>
-				<div class="flex gap-10 items-center">
-					<StatCard label="Requests" value={formattedRequests} />
-					<StatCard label="Bandwidth" value={formattedBandwidth} />
-				</div>
-			</div>
+			<p class=" mb-90">
+				Report from {formattedDate}
+			</p>
 			<h1 class="heading-1 w-grid-9 mb-45">{data.summary.message}</h1>
-
 		</header>
 	);
 }
